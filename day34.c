53438a169aa5a60b48ee10640cd5916b44f91151
rahulgarbyal <rahulgarbyal15@gmail.com>
2026-03-24 21:40:43 +0530

    added day34 code!!

---
+++ day34.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+struct node {
+    int data;
+    struct node* next;
+};
+
+struct node* top = NULL;
+
+void push(int x) {
+    struct node* temp = (struct node*)malloc(sizeof(struct node));
+    temp->data = x;
+    temp->next = top;
+    top = temp;
+}
+
+int pop() {
+    if (top == NULL) {
+        printf("Stack Underflow\n");
+        return -1;
+    }
+    struct node* temp = top;
+    int val = temp->data;
+    top = top->next;
+    free(temp);
+    return val;
+}
+
+int evaluate(char exp[]) {
+    int i = 0;
+
+    while (exp[i] != '\0') {
+
+        if (exp[i] == ' ') {
+            i++;
+            continue;
+        }
+
+        if (isdigit(exp[i])) {
+            int num = 0;
+
+            while (isdigit(exp[i])) {
+                num = num * 10 + (exp[i] - '0');
+                i++;
+            }
+
+            push(num);
+            i--; 
+        }
+        
+        else {
+            int val2 = pop();
+            int val1 = pop();
+
+            switch (exp[i]) {
+                case '+': push(val1 + val2); break;
+                case '-': push(val1 - val2); break;
+                case '*': push(val1 * val2); break;
+                case '/': push(val1 / val2); break;
+            }
+        }
+        i++;
+    }
+
+    return pop();
+}
+
+int main() {
+    char exp[100];
+
+    printf("Enter postfix expression: ");
+    fgets(exp, sizeof(exp), stdin);
+
+    int result = evaluate(exp);
+
+    printf("Result = %d\n", result);
+
+    return 0;
+}
