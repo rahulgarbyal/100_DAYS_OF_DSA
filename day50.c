e053031e9bc8af5d13afe70a796c809edc0550a0
rahulgarbyal <rahulgarbyal15@gmail.com>
2026-04-06 23:41:10 +0530

    added day50 code!!

---
+++ day50.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+struct Node* search(struct Node* root, int key) {
+    if (root == NULL || root->data == key)
+        return root;
+
+    if (key < root->data)
+        return search(root->left, key);
+
+    return search(root->right, key);
+}
+
+int main() {
+    struct Node* root = createNode(50);
+    root->left = createNode(30);
+    root->right = createNode(70);
+    root->left->left = createNode(20);
+    root->left->right = createNode(40);
+    root->right->left = createNode(60);
+    root->right->right = createNode(80);
+
+    int key = 60;
+    struct Node* result = search(root, key);
+
+    if (result != NULL)
+        printf("Found");
+    else
+        printf("Not Found");
+
+    return 0;
+}
